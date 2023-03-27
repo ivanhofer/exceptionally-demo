4898f7c71173fac26fb8d4842fdf05bfe2c53436
Ivan Hofer <ivan.hofer@outlook.com>
2023-03-27 19:18:48 +0200

    throw errors where fetch can go wrong

--- packages/utils.ts
+++ packages/utils.ts
@@ -1,2 +1,27 @@
-export const typedJsonFetch = <Data>(...args: Parameters<typeof fetch>) =>
-  fetch(...args).then((response) => response.json() as Promise<Data>);
+const matchesSchema = (data: unknown) => !!data;
+
+export const typedJsonFetch = async <Data>(
+  ...args: Parameters<typeof fetch>
+) => {
+  const response = await fetch(...args).catch((e) => {
+    console.error(e);
+    throw new Error(`Network error`);
+  });
+
+  if (!response.ok) {
+    // non 200 status codes
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const data = await response.json().catch((e) => {
+    console.error(e);
+    throw new Error(`Invalid JSON`);
+  });
+
+  if (!matchesSchema(data)) {
+    // usually you would do some kind of data validation here (e.g. using `zod`)
+    throw new Error(`Data does not match expected schema`);
+  }
+
+  return data as Data;
+};
